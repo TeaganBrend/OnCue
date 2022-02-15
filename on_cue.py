ae41b7a246f1f1b015ec407ad6bd799b2ba7bd4b
Madeline Marlett <madelinem11765@gmail.com>
2022-02-14 21:36:00 -0500

    removed imports

--- on_cue.py
+++ on_cue.py
@@ -1,15 +1,9 @@
-from audioop import tomono
-import os
 from random import sample
-from tkinter import Grid
-from turtle import bgcolor
 from pysinewave import SineWave
-import time
 from kivy.clock import Clock
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.slider import Slider
